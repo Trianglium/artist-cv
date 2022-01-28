49779a914e7a0035bb8f56b6887e45eea713b434
Trianglium <70343977+Trianglium@users.noreply.github.com>
2022-01-27 21:08:54 -0600

    Update Configuration in Manage and WSGI

--- lsite/lsite/wsgi.py
+++ lsite/lsite/wsgi.py
@@ -12,5 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lsite.settings')
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
