c814f2ac5b5379ecd1858a7d5d4ab0a75dcdcf8b
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-03 13:48:25 -0600

    Project app to replace Portfolio app's intended purpose

---
+++ cv/projects/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Project(models.Model):
+    # Title of the Project, give it a unique name.
+    title = models.CharField(max_length=100, unique=True)
+    # Use Summary as a preview for your post.
+    # People viewing your site will see the summary before they decide whether or not to view the Project details.
+    # Similar to what you see on a google results page before deciding to click on a website result.
+    summary = models.Charfield(max_length=155)
+    # Description of the project (in depth, or not, use as you please).
+    # This is where you describe the actual  details of everything.
+    # There is no set max length for the description field.
+    description = models.TextField()
+    # Use the skill field as a sort of ‘tag’ for what the project is about.
+    # It’s recommended that skill names remain consistent as to help with navigating the site.
+    skill = models.CharField(max_length=20)
+    # Optional Image of or for Project
+    image = models.FilePathField(path="/img", null=True)
+    # Optional Audio / Sound for or of Project
+    # Intended for Musicians and Sound Related Artistis
+    sound = models.FilePathField(path=“/audio”, null=True)
+    # Optional video / clip for or of  project
+    video = models.FilePathField(path=“/vid”, null=True)
