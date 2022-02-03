7e3950ee1ee402ac2f8d021452b3c1e4c22dc2db
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-03 13:52:35 -0600

    Class Based Views for Projects

--- cv/projects/views.py
+++ cv/projects/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from projects.models import Project
+
+class ProjectIndexView(generic.ListView):
+    model = Project
+    template_name = 'projects/project_index.html'
+
+
+
+class ProjectDetailView(generic.DetailView):
+    model = Project
+    template_name = 'projects/project_detail.html'
