b4c2426a385fdb0d49419f4353bc4cc75ee9ff12
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-12 09:49:13 -0600

    Implement Blog Models (Post/Comment/Tag)

---
+++ cv/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.value
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True,  db_index=True)
+    modified_at = models.DateTimeField(auto_now=True,  db_index=True)
+    published_at = models.DateTimeField(blank=True, null=True, db_index=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField(unique=True)
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
