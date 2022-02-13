0e912ba3af2a62e6c3b897528bd71ce40936d5f3
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-13 15:38:20 -0600

    Register New Project Models

--- cv/projects/admin.py
+++ cv/projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from projects.models import Project
+from projects.models import Project, Link, Content
 
 
 admin.site.register(Project)
+admin.site.register(Link)
+admin.site.register(Content)
