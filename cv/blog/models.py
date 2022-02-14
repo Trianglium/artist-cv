00e4e601c144ddb8dac96a26597f2f04c37b4421
Jordan <jbrockerrudow@gmail.com>
2022-02-14 14:00:53 +0000

    Implement Blog Application

---
+++ cv/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# TO DO: Finish Blog Models and uncomment them
+"""
+class Post(models.Model):
+    title = CharField(max_length=155, editable=True)
+    intro = TextField(null=True, editable=True)
+    body = TextField(null=True, editable=True)
+    conclusion = TextField(null=True, editable=True)
+    genre =
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    created =
+    modified =
+    sub_blog = models.
+    votes = models.IntegerField(
+"""
