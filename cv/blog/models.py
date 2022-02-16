a85025dc350a57e7461b0b602bc78b79816b46c6
Jordan <jbrockerrudow@gmail.com>
2022-02-16 13:22:46 +0000

    Blog Models, Views and Commentform

--- cv/blog/models.py
+++ cv/blog/models.py
@@ -1,15 +1,49 @@
 from django.db import models
-# TO DO: Finish Blog Models and uncomment them
-"""
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.value
+
+
 class Post(models.Model):
-    title = CharField(max_length=155, editable=True)
-    intro = TextField(null=True, editable=True)
-    body = TextField(null=True, editable=True)
-    conclusion = TextField(null=True, editable=True)
-    genre =
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    created =
-    modified =
-    sub_blog = models.
-    votes = models.IntegerField(
-"""
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True,  db_index=True)
+    modified_at = models.DateTimeField(auto_now=True,  db_index=True)
+    published_at = models.DateTimeField(blank=True, null=True, db_index=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField(unique=True)
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
+
+    def __str__(self):
+        return self.title
+# AuthorProfile as to separate info from the User Auth Model
+# Allowing for a separation of concerns
+# And added security.
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.__class__.__name__} object for {self.user}"
