5a1764d25c00879837edb52f248b4902c01f5b2b
Jordan <jbrockerrudow@gmail.com>
2022-02-17 15:42:26 +0000

    Update Models for About Sections

--- cv/about/models.py
+++ cv/about/models.py
@@ -8,32 +8,16 @@ from django.contrib.auth.models import User
 
 # About Section Model; Customizable.
 # Example About Sections: "Work Experience", "Education and Certification", "Technologies and Languages", "Skills", "Objective", etc.
-class Section(models.Model):
-    # subject as in, the way police talk about a person of interest.
-    # synonyms for this field are "owner", "author", "me" (in "about me"), "person", etc.
-    subject = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=155, null=True, editable=True)
-    body = models.TextField(null=True, editable=True)
+class CustomSection(models.Model):
+    head = models.CharField(max_length=155, null=True, editable=True)
+    body = models.TextField(max_length=2000, null=True, editable=True)
+    section_subject = models.ManyToManyField(Subject, related_name="sections")
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now=True)
 
 
-
-
-
-# TO DO - Integrate Custom Auth User Model into Contact Model
-# Would be beneficial if the default for some of these these fields
-# could be the required Auth User Model Fields
-# Idea - Should Contact Model just have generic fields
-# to let the user input their contact method and value?
-
-class Contact(models.Model):
-    email = models.EmailField(null=True, editable=True),
-    number = models.IntegerField(null=True, editable=True)
-    subject = models.CharField(max_length=255, null=True, editable=False)
-    body = models.CharField(max_length=800, null=True, editable=False)
-
+class Subject(models.Model):
+    name = models.TextField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.subject
-
+        return self.value
