22215a137a2885c646a3b2e5c55d68f35871fa4c
Jordan <jbrockerrudow@gmail.com>
2022-02-18 14:46:23 +0000

    Update Verbose Names on Resume Models

--- cv/resume/models.py
+++ cv/resume/models.py
@@ -6,7 +6,11 @@ from base.models import Organization, Skill
 
 class Professional(Organization):
     # Inherits all fields from Organization
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, help_text='Job Title or Professional Title')
+
+    class Meta:
+        verbose_name = 'Professional Experience'
+        verbose_name_plural = 'Professional Experience'
 
     def __str__(self):
         return self.title
@@ -15,31 +19,47 @@ class Professional(Organization):
 class Educational(Organization):
     # Inherits all fields from Organization
     # .credential: (Optional field) Meant for credentials. IE degree, diploma, certificate, etc.
-    credential = models.CharField(max_length=255, null=True)
+    credential = models.CharField(max_length=255, null=True, help_text='Enter Credentials here. Degrees, Certifications, Licenses, Diplomas, Etc.')
     # .secondary_credential: minor, secondary area of focus, specialization, or -
     # other courses/certificates that are related to the main credential
-    secondary_credentials = models.TextField(max_length=500, null=True)
+    secondary_credentials = models.TextField(max_length=500, null=True, help_text='Enter related course, certificates, or minor credentials that are related to the input above. IE a Minor in Pyschology')
     # .url: (Optional field) meant for certifications and other. URL to validate or URL to view the credential
-    url = models.URLField(max_length=500, null=True)
+    url = models.URLField(max_length=500, null=True, help_text='This field is meant for certifications and other credentials that offer a url to validate the credential')
+
+    class Meta:
+        verbose_name = 'Education/Certification'
+        verbose_name_plural = 'Education/Certification'
 
     def __str__(self):
         return self.credential
 
 class Volunteer(Organization):
     # Inherits all fields from Organization
+    class Meta:
+        verbose_name = 'Volunteer Experience'
+        verbose_name_plural = 'Volunteer Experience'
+
     def __str__(self):
         return self.institution
 
 
 class Technical(Skill):
     # Inherits all fields from Skill
+    class Meta:
+        verbose_name = 'Hard Skill'
+        verbose_name_plural = 'Hard Skills'
+
     def __str__(self):
-        return self.name
+        return self.skill_name
 
 class Interpersonal(Skill):
     # Inherits all fields from Skill
+    class Meta:
+        verbose_name = 'Soft Skill'
+        verbose_name_plural = 'Soft Skills'
+
     def __str__(self):
-        return self.name
+        return self.skill_name
 
 
 
@@ -49,16 +69,10 @@ class ProfessionalProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="resume"
     )
-    objective = models.TextField(null=True, editable=True)
-    resume_summary = models.TextField(null=True, editable=True)
-    public_email = models.TextField(null=True, editable=True)
-    resume_link = models.TextField(null=True, editable=True, help_text="Upload your resume to Google Drive and Share the File with anyone who has the link. Copy and Paste the link into this field")
-
-    def download_resume(self):
-        link = str(self.resume_link).split('file/d')
-        base = f'{link[0]}uc?export=download&id={}'
-
-        return
+    objective = models.TextField(null=True, editable=True, help_text='(Optional) Resume Objective, generally found at the top of a resume/cv.')
+    resume_summary = models.TextField(null=True, editable=True, help_text='(Optional) Resume Summary Section')
+    public_email = models.TextField(null=True, editable=True, help_text='(Recommended) Enter the email employers should contact you at. This will be public and can be different from the signup email if you so choose.')
+    resume_link = models.TextField(null=True, editable=True, help_text='(Recommended) Upload your resume to Google Drive and Share the File with anyone who has the link. Copy and Paste the link into this field')
 
 
 
