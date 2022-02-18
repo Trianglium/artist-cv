79bed6ef4fcf98648f2f08b2883ce908a7f47524
Jordan <jbrockerrudow@gmail.com>
2022-02-18 13:17:01 +0000

    Register Resume Models

--- cv/resume/admin.py
+++ cv/resume/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from resume.models import ProfessionalProfile, Professional, Educational, Volunteer, Technical, Interpersonal
 
-# Register your models here.
+
+admin.site.register(ProfessionalProfile)
+
+admin.site.register(Professional)
+
+admin.site.register(Educational)
+admin.site.register(Volunteer)
+
+admin.site.register(Technical)
+admin.site.register(Interpersonal)
+
+
