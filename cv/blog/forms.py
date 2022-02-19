c90b4e20832b69ce6e8c099703a284065e48e2df
Jordan <jbrockerrudow@gmail.com>
2022-02-19 14:14:49 +0000

    Modelforms for Blog

--- cv/blog/forms.py
+++ cv/blog/forms.py
@@ -1,17 +1,33 @@
-from django import forms 
+from django import forms
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-from blog.models import Comment
+from blog.models import Comment, Post, Tag, AuthorProfile
 
 
 class CommentForm(forms.ModelForm):
   class Meta:
-    model = Comment 
+    model = Comment
     fields = ["content"]
 
-  def __init__(self, *args, **kwargs): 
-    super(CommentForm, self).__init__(*args, **kwargs) 
+  def __init__(self, *args, **kwargs):
+    super(CommentForm, self).__init__(*args, **kwargs)
     self.helper = FormHelper()
     self.helper.add_input(Submit('submit', 'Submit'))
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ["title", "summary", "content", "tags"]
+
+class TagForm(forms.ModelForm):
+  class Meta:
+    model = Tag
+    fields = ["value"]
+
+class AuthorProfileForm(forms.ModelForm):
+    class Meta:
+        model = AuthorProfile
+        fields = ["bio"]
+# https://docs.djangoproject.com/en/dev/ref/contrib/admin/#django.contrib.admin.ModelAdmin.formfield_for_manytomany
--- cv/blog/models.py
+++ cv/blog/models.py
@@ -13,6 +13,9 @@ class Comment(BaseContent):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
 
+    def __str__(self):
+        return self.creator
+
 class Tag(models.Model):
     value = models.TextField(max_length=100, unique=True)
 
@@ -22,9 +25,10 @@ class Tag(models.Model):
 
 class Post(BaseArticle):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    # db_index is required. do not remove.
     published_at = models.DateTimeField(blank=True, null=True, db_index=True)
     slug = models.SlugField(unique=True)
-    tags = models.ManyToManyField(Tag, related_name="posts")
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
     comments = GenericRelation(Comment)
 
     def __str__(self):
@@ -36,7 +40,11 @@ class AuthorProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
     )
-    bio = models.TextField()
+    bio = models.TextField(
+        null=True,
+        blank=True,
+        help_text='Optional Author Bio. Separate From Professional Profile Resume Summary and Object',
+    )
 
     def __str__(self):
         return f"{self.__class__.__name__} object for {self.user}"
