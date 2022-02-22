1fade44b1eba58248ea3c868ad3bb3d472d41c96
Jordan <jbrockerrudow@gmail.com>
2022-02-22 18:57:13 +0000

    CKEditor URL config

--- cv/cv/urls.py
+++ cv/cv/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     #path('resume/', include('resume.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('ckeditor_uploader/', include('ckeditor_uploader.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
