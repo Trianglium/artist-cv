a9ae0846f80a2a95f75518a800dcb30334947d87
Jordan <jbrockerrudow@gmail.com>
2022-02-23 14:37:38 +0000

    CRUD Forms

--- cv/resume/forms.py
+++ cv/resume/forms.py
@@ -1,16 +1,36 @@
-from django import forms 
+from django import forms
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 from resume.models import Professional, Educational, Volunteer, Technical, Interpersonal, ProfessionalProfile
 
-#class CommentForm(forms.ModelForm):
-    #class Meta:
-        #model = Comment
-        #fields = ["content"]
-    
-    #def __init__(self, *args, **kwargs):
-        #super(CommentForm, self).__init__(*args, **kwargs)
-        #self.helper = FormHelper()
-        #self.helper.add_input(Submit('submit', 'Submit'))
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Professional
+        exclude = ['created_at', 'modified_at']
+
+    def __init__(self, *args, **kwargs):
+        super(JobForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+class EducationalForm(forms.ModelForm):
+    class Meta:
+        model = Professional
+        exclude = ['created_at', 'modified_at']
+
+    def __init__(self, *args, **kwargs):
+        super(JobForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+class VolunteerForm(forms.ModelForm):
+    class Meta:
+        model = Professional
+        exclude = ['created_at', 'modified_at']
+
+    def __init__(self, *args, **kwargs):
+        super(VolunteerForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
