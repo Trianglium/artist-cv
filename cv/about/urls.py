d34ce3772c8f86591cba210ee22877ad272ad41a
Jordan <jbrockerrudow@gmail.com>
2022-02-23 14:39:20 +0000

    Route url patterns for list/crud/contact

--- cv/about/urls.py
+++ cv/about/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 app_name = 'about'
 urlpatterns = [
-    path('', views.EntryListView.as_view(), name='all'),
-    path('new/entry/', views.EntryCreateView.as_view(), name='new_entry'),
-    path('edit/<int:pk>/', views.EntryUpdateView.as_view(), name='edit_entry'),
-    path('delete/<int:pk>/', views.EntryDeleteView.as_view(), name='delete_entry'),
-    path('new/section/', views.SectionUpdateView.as_view(), name='new_section'),
-    path('edit/section/<int:pk>/', views.SectionUpdateView.as_view(), name='edit_section'),
-    path('delete/section/<int:pk>/', views.SectionDeleteView.as_view(), name='delete_section'),
+    path('resume/', views.EntryListView.as_view(), name='all'),
+    path('resume/new/entry/', views.EntryCreateView.as_view(), name='new_entry'),
+    path('resume/edit/entry/<int:pk>/', views.EntryUpdateView.as_view(), name='edit_entry'),
+    path('resume/delete/entry/<int:pk>/', views.EntryDeleteView.as_view(), name='delete_entry'),
+    path('resume/new/section/', views.SectionUpdateView.as_view(), name='new_section'),
+    path('resume/edit/section/<int:pk>/', views.SectionUpdateView.as_view(), name='edit_section'),
+    path('resume/delete/section/<int:pk>/', views.SectionDeleteView.as_view(), name='delete_section'),
+    path('contact/', views.ContactFormView.as_view(), name='connect'),
 
 ]
