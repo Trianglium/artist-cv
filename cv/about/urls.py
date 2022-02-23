c864c8bc572cf96cec9f6cea840b862227b98867
Jordan <jbrockerrudow@gmail.com>
2022-02-23 14:00:39 +0000

    CRUD URL Patterns

--- cv/about/urls.py
+++ cv/about/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 app_name = 'about'
 urlpatterns = [
+    path('', views.EntryListView.as_view(), name='all'),
+    path('new/entry/', views.EntryCreateView.as_view(), name='new_entry'),
+    path('edit/<int:pk>/', views.EntryUpdateView.as_view(), name='edit_entry'),
+    path('delete/<int:pk>/', views.EntryDeleteView.as_view(), name='delete_entry'),
+    path('new/section/', views.SectionUpdateView.as_view(), name='new_section'),
+    path('edit/section/<int:pk>/', views.SectionUpdateView.as_view(), name='edit_section'),
+    path('delete/section/<int:pk>/', views.SectionDeleteView.as_view(), name='delete_section'),
+
 ]
