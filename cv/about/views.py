7c37ac119ce409e1a37c49ab2d9c498aaeb6b8a2
Jordan <jbrockerrudow@gmail.com>
2022-02-23 14:02:57 +0000

    Updates to CRUD Views - crispy forms & ckeditor

--- cv/about/views.py
+++ cv/about/views.py
@@ -5,15 +5,31 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Section, Entry
+from .forms import EntryForm, SectionForm
 
 class EntryListView(ListView):
      model = Entry
      fields = ['content', 'resume_section', 'start_date', 'end_date']
      template_name = 'about/main.html'
 
+     def get_context_data(self, **kwargs):
+         context = super(EntryListView, self).get_context_data(**kwargs)
+         context.update({
+             'entry_list': Entry.objects.all(),
+             'section_list': Section.objects.all(),
+         })
+         return context
+
      def get_queryset(self):
          return Entry.objects.order_by('resume_section')
 
+class SectionListView(ListView):
+    model = Section
+    fields = ['title']
+    template_name = 'about/section_list.html'
+
+    def get_queryset(self):
+        return Section.objects.all()
 
 
 class SectionCreateView(CreateView, LoginRequiredMixin):
