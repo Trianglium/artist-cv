10137d00ecea50dfd40cce996806109b9c566abc
Jordan <jbrockerrudow@gmail.com>
2022-02-23 14:23:59 +0000

    Resume Views - crud/list/detail

--- cv/resume/views.py
+++ cv/resume/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def resume_index(request):
+    template = loader.get_template('resume/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
